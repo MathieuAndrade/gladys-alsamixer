4fa087ccfd5f3dadcf58da64b26732740044b4c3
LeptitGeek <mxflash1996@gmail.com>
2018-06-26 21:08:42 +0200

    Add init function and params

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var install = require('./lib/install.js');
-var uninstall = require('./lib/uninstall.js');
-var command = require('./lib/alsamixer.command.js');
-var exec = require('./lib/exec.js');
-
 module.exports = function(sails) {
+
+	const install = require('./lib/install.js')
+	const uninstall = require('./lib/uninstall.js')
+	const command = require('./lib/alsamixer.command.js')
+	const exec = require('./lib/exec.js')
+	const init = require('./lib/init.js')
+
+	gladys.on('ready', function(){
+        init();
+    });
 
 	return {
 		install,
